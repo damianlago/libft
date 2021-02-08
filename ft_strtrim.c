ac6d577f8916cd5d2db2e0aadcebc7c9071af874
Damian Lago <damianlagomail@gmail.com>
2021-02-08 13:36:09 +0100

    update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,21 +11,19 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*s2;
-	int		i;
+	int	i;
 
-	if (s1 == NULL || set == NULL)
+	if (!s1 || !set)
 		return (NULL);
-	while (ft_strchr(set, *s1) != NULL)
+	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	i = ft_strlen(s1);
-	while (ft_strchr(set, s1[i]) != NULL)
+	while (i && ft_strchr(set, s1[i]))
 		i--;
-	s2 = malloc((i + 1) * sizeof(char));
 	s2 = ft_substr(s1, 0, i + 1);
 	return (s2);
 }
--- libft.h
+++ libft.h
@@ -43,6 +43,7 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
