2286d87167ce9fff3bca928beee6c26224df1643
Damian Lago moran <dlago-mo@c1r8s3.42madrid.com>
2021-04-13 15:13:34 +0200

    done

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,14 +6,12 @@
 /*   By: dlago-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 15:55:59 by dlago-mo          #+#    #+#             */
-/*   Updated: 2021/04/13 13:47:15 by dlago-mo         ###   ########.fr       */
+/*   Updated: 2021/04/13 14:41:38 by dlago-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+
 size_t	ft_len(long nb)
 {
 	size_t	len;
@@ -42,7 +40,8 @@ char	*ft_itoa(int n)
 
 	nb = n;
 	len = ft_len(nb);
-	if (!(s = malloc(sizeof(char) * (1 + len))))
+	s = malloc(sizeof(char) * (1 + len));
+	if (s == NULL)
 		return (NULL);
 	s[len--] = '\0';
 	if (nb == 0)
@@ -60,11 +59,4 @@ char	*ft_itoa(int n)
 		nb /= 10;
 	}
 	return (s);
-}
-
-int		main(void)
-{
-	printf("%s", ft_itoa(47));
-	//sytem.out("Leaks");
-	return (0);
 }
--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,15 @@
 /*   By: dlago-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 11:54:25 by dlago-mo          #+#    #+#             */
-/*   Updated: 2021/02/10 15:17:33 by dlago-mo         ###   ########.fr       */
+/*   Updated: 2021/04/13 15:13:14 by dlago-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int			wrd_nb(char const *s, char c)
+int	wrd_nb(char const *s, char c)
 {
 	int		count;
 
@@ -37,7 +39,7 @@ static char	**ft_free(char **s2, int count)
 	return (0);
 }
 
-char		**ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
 	char	**s2;
 	int		i;
@@ -46,7 +48,8 @@ char		**ft_split(char const *s, char c)
 
 	i = -1;
 	cont = 0;
-	if (!s || !(s2 = (char **)malloc(sizeof(char *) * (wrd_nb(s, c) + 1))))
+	s2 = (char **)malloc(sizeof(char *) * (wrd_nb(s, c) + 1));
+	if (!s || !s2)
 		return (0);
 	while (s[++i] != '\0')
 	{
@@ -55,7 +58,8 @@ char		**ft_split(char const *s, char c)
 			j++;
 		if (j > i)
 		{
-			if (!(s2[cont] = (char *)malloc(sizeof(char) * (j - i + 1))))
+			s2[cont] = (char *)malloc(sizeof(char) * (j - i + 1));
+			if (s2[cont] == NULL)
 				return (ft_free(s2, cont));
 			ft_strlcpy(s2[cont++], s + i, j - i + 1);
 			i = j - 1;
@@ -63,4 +67,17 @@ char		**ft_split(char const *s, char c)
 	}
 	s2[cont] = 0;
 	return (s2);
+}
+
+int	main(void)
+{
+	int i = 0;
+	char const s[] = "Hhhhahhh";
+	char c = 'a';
+	char **test = ft_split(s, c);
+	while (i < 2)
+	{
+		printf("%s\n", test[i]);
+		i++;
+	}
 }
