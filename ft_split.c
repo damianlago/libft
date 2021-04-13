f6824f1c521c377fd558c11476934ff7ddf2d024
Damian Lago moran <dlago-mo@c1r8s3.42madrid.com>
2021-04-13 17:30:48 +0200

    done

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dlago-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 11:54:25 by dlago-mo          #+#    #+#             */
-/*   Updated: 2021/04/13 16:26:57 by dlago-mo         ###   ########.fr       */
+/*   Updated: 2021/04/13 17:27:09 by dlago-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,13 @@ int	wrd_nb(char const *s, char c)
 	return (count);
 }
 
+int	wrd_len(char const *s, char c, int j)
+{
+	while (s[j] != c && s[j] != '\0')
+		j++;
+	return (j);
+}
+
 static char	**ft_free(char **s2, int count)
 {
 	int		i;
@@ -37,22 +44,25 @@ static char	**ft_free(char **s2, int count)
 	return (0);
 }
 
-char	**ft_return_wrd(char const *s, char c, char **s2)
+char	**ft_split(char const *s, char c)
 {
-	int	i;
-	int	j;
-	int	cont;
+	int		i;
+	int		j;
+	int		cont;
+	char	**s2;
 
 	i = -1;
 	cont = 0;
+	s2 = (char **)malloc(sizeof(char *) * (wrd_nb(s, c) + 1));
+	if (!s || !s2)
+		return (0);
 	while (s[++i] != '\0')
 	{
 		j = i;
-		while (s[j] != c && s[j] != '\0')
-			j++;
+		j = wrd_len(s, c, j);
 		if (j > i)
 		{
-			s2[cont] = (char *)malloc(sizeof(char *) * (j - i + 1));
+			s2[cont] = (char *)malloc(sizeof(char) * (j - i + 1));
 			if (!s2[cont])
 				return (ft_free(s2, cont));
 			ft_strlcpy(s2[cont++], s + i, j - i + 1);
@@ -61,14 +71,4 @@ char	**ft_return_wrd(char const *s, char c, char **s2)
 	}
 	s2[cont] = 0;
 	return (s2);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**s2;
-
-	s2 = (char **)malloc(sizeof(char *) * (wrd_nb(s, c) + 1));
-	if (!s || !s2)
-		return (0);
-	return (ft_return_wrd(s, c, s2));
 }
